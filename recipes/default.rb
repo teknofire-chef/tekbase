d9f7071495d24262b23106ae290c77e51feefe70
Will Fisher <wfisher@chef.io>
2018-09-03 21:03:07 -0700

    base systems

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+include_recipe 'chef-client::default'
+
+node.default['teknofire']['chef-client-version'] = '14.4.56'
+
+chef_client_updater 'Install latest' do
+  version node['teknofire']['chef-client-version']
+end
+
+user 'teknofire' do
+  manage_home true
+  shell '/bin/bash'
+  action [:create, :manage]
+end
+
+sudo 'admin' do
+  user 'teknofire'
+  nopasswd true
+end
